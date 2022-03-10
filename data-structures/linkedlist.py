c8b6d9b76ca544c22c65333dfe04f451107ecaf2
jissac <jaeeysuk@gmail.com>
2022-03-10 16:36:13 -0600

    linked list

--- data-structures/linkedlist.py
+++ data-structures/linkedlist.py
@@ -1,33 +1,36 @@
-'''
+"""
 # manual implementation Singly Linked List
-'''
+"""
 
-class Node():
+
+class Node:
     def __init__(self, value):
         self.val = value
         self.next = None
 
-n1 = Node('A')
-n2 = Node('B')
-n3 = Node('C')
+
+n1 = Node("A")
+n2 = Node("B")
+n3 = Node("C")
 
 n1.next = n2
 n2.next = n3
 
 # number of nodes in linked list
-def nodeTraverse(head:Node) -> int:
+def nodeTraverse(head: Node) -> int:
     count = 0
     current = head
     while current.next != None:
         print(current.val)
-        count+=1
+        count += 1
         current = current.next
     return count
+
 
 print(f"Number of Nodes: {nodeTraverse(n1)}")
 
 # reverse the nodes in linked list
-def reverseLinkedList(head:Node) -> Node:
+def reverseLinkedList(head: Node) -> Node:
     prev = None
     current = head
     while current != None:
@@ -36,12 +39,14 @@ def reverseLinkedList(head:Node) -> Node:
         prev = current
         current = next
     return prev
+
+
 # None      A       ->      B       ->      C
 # prev      current         next
 new_head = reverseLinkedList(n1)
 print(f"Reversed Linked List New Head Nde: {new_head} with value: {new_head.val}")
 
 
-'''
+"""
 # Doubly Linked List
-'''
+"""
